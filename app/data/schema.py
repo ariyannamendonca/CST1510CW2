e1bffdb4b43d24a5f085b63e70aef578f6101c7f
ariyanna <AM4287@live.mdx.ac.uk>
2025-11-24 16:17:37 +0400

    made main.py and fixed/made the tables for all columns

--- app/data/schema.py
+++ app/data/schema.py
@@ -2,11 +2,55 @@ def create_users_table(conn):
     """Create users table."""
     cursor = conn.cursor()
     cursor.execute("""
-        CREATE TABLE IF EXISTS users (
+        CREATE TABLE IF NOT EXISTS users (
                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
                    username TEXT NOT NULL UNIQUE,
                    password_hash TEXT NOT NULL,
                    role TEXT DEFAULT 'users'
+        )
+    """)
+    conn.commit()
+
+def create_cyber_incidents_table(conn):
+    """Create cyber incidents table."""
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS cyber_incidents (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    title TEXT NOT NULL,
+                    severity TEXT NOT NULL,
+                    status TEXT DEFAULT 'open',
+                    date TEXT
+        )
+    """)
+    conn.commit()
+
+def create_datasets_metadata_table(conn):
+    """Create datasets_metadata table."""
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS datasets_metadata (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                   name TEXT NOT NULL,
+                   source TEXT,
+                   category TEXT,
+                   size INTEGER,
+                   created_date
+        )
+    """)
+    conn.commit()
+
+def create_it_tickets_table(conn):
+    """Create it_tickets table."""
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS it_tickets (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    title TEXT NOT NULL,
+                    priority TEXT NOT NULL,
+                    status TEXT DEFAULT 'open',
+                    created_date TEXT,
+                    description TEXT
         )
     """)
     conn.commit()
